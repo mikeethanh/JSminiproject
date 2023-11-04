ef619fd2a15242b199ef06535df28485e7a56cae
mikeethanh <109719604+mikeethanh@users.noreply.github.com>
2023-11-04 13:11:18 +0700

    addAPI

--- callAPI_in_weatherApp/main.js
+++ callAPI_in_weatherApp/main.js
@@ -1,9 +1,46 @@
-var search = document.querySelector('.search')
-var city = document.querySelector('.city')
-var country = document.querySelector('.country')
-var value = document.querySelector('.value')
-var shortDesc = document.querySelector('.short-desc')
-var visibility = document.querySelector('.visibility span')
-var wind = document.querySelector('.wind span')
-var sun = document.querySelector('.sun span')
-var search = document.querySelector('.search')
+const input = document.querySelector('.input-search')
+
+function changeWeatherUI(weather) {
+	const city = document.querySelector('.name .city')
+	const country = document.querySelector('.name .country')
+	const time = document.querySelector('.time')
+	const temperature = document.querySelector('.temperature .value')
+	const shortDesc = document.querySelector('.short-desc')
+
+	const visibility = document.querySelector('.visibility span')
+	const wind = document.querySelector('.wind span')
+	const cloud = document.querySelector('.cloud span')
+
+	city.innerHTML = weather.name
+	country.innerHTML = weather.sys.country
+	time.innerHTML = new Date().toLocaleString()
+	shortDesc.innerHTML = weather.weather[0].main
+
+	const temp = Math.round(weather.main.temp)
+	temperature.innerHTML = temp
+
+	temp >= 18
+		? (document.body.className = 'hot')
+		: (document.body.className = 'cold')
+
+	visibility.innerHTML = weather.visibility + ' (m)'
+	wind.innerHTML = weather.wind.speed + ' (m/s)'
+	cloud.innerHTML = weather.clouds.all + ' (%)'
+}
+
+input.addEventListener('keyup', (e) => {
+	if (e.keyCode === 13) {
+		getWeather(e.target.value)
+	}
+})
+
+async function getWeather(input) {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=3f981f0c1fa8fb9aa6dfb6e589efd054`
+
+	const res = await fetch(url)
+	const weather = await res.json()
+
+	changeWeatherUI(weather)
+}
+
+getWeather('ha noi')
